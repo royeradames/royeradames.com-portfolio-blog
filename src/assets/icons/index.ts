2fe6fe277effcad3dee0352046e128a82f6c17a4
royeradames <raadamesbonilla@pplweb.com>
2024-12-15 03:27:05 -0400

    import and pass svg has props

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,19 @@
+import AddressIcon from './address-icon.svg';
+import EmailIcon from './email-icon.svg';
+import EnglishIcon from './english-icon.svg';
+import GitHubIcon from './github-icon.svg';
+import LinkedInIcon from './linkedin-icon.svg';
+import SpanishIcon from './spanish-icon.svg';
+import TwitterIcon from './twitter-icon.svg';
+import WebsiteIcon from './website-icon.svg';
+
+export {
+  AddressIcon,
+  EmailIcon,
+  EnglishIcon,
+  GitHubIcon,
+  LinkedInIcon,
+  SpanishIcon,
+  TwitterIcon,
+  WebsiteIcon,
+};
