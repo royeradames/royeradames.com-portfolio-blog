bb6ffa5f7ac656fd57e7f47a84b8bfc104dd26de
royeradames <raadamesbonilla@pplweb.com>
2024-12-15 15:13:24 -0400

    init latest projects

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,7 @@
 import AddressIcon from './address-icon.svg';
 import EmailIcon from './email-icon.svg';
 import EnglishIcon from './english-icon.svg';
+import FigmaIcon from './figma-icon.svg';
 import GitHubIcon from './github-icon.svg';
 import LinkedInIcon from './linkedin-icon.svg';
 import SpanishIcon from './spanish-icon.svg';
@@ -11,6 +12,7 @@ export {
   AddressIcon,
   EmailIcon,
   EnglishIcon,
+  FigmaIcon,
   GitHubIcon,
   LinkedInIcon,
   SpanishIcon,
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,30 @@ export default {
       backgroundImage: {
         'gradient-primary': 'linear-gradient(90deg, #A478E8 0%, #516CF7 100%)',
       },
+      colors: {
+        gray: {
+          dark: '#47516B',
+          darker: '#2E2E48',
+          darkest: '#232339',
+          DEFAULT: '#79819A',
+          light: '#ACB1C3',
+          lighter: '#D9DFE8',
+          lightest: '#E2E6EE',
+        },
+        primary: {
+          dark: '#5531A7',
+          light: '#C696FC',
+        },
+        secondary: {
+          dark: '#2A337E',
+          light: '#95AAFB',
+          lighter: '#E1E7FE',
+        },
+        social: {
+          linkedin: '#0077B5',
+          twitter: '#00ACEE',
+        },
+      },
     },
   },
   plugins: [
@@ -18,6 +42,7 @@ export default {
           color: 'transparent',
         },
       });
+	  
     },
   ],
 };
