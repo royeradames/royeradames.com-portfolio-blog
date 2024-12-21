1bf286029bd415a0108e6b7f1ea7effe37501c69
royeradames <raadamesbonilla@pplweb.com>
2024-12-20 23:52:48 -0400

    complete  experience

--- src/assets/business-logos/index.ts
+++ src/assets/business-logos/index.ts
@@ -1,4 +1,6 @@
 import AppleLogo from './apple.svg';
+import GoogleLogo from './google.svg';
 import TeslaLogo from './tesla.svg';
+import VectornatorLogo from './vectornator.svg';
 
-export { AppleLogo, TeslaLogo };
+export { AppleLogo, GoogleLogo, TeslaLogo, VectornatorLogo };
